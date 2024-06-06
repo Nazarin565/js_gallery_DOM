20a3b79dfad6d26852d502cbb4354607e73e3fe4
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-06-06 10:43:19 +0300

    make build

---
+++ index.20697a47.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target.closest(".list-item__link");t.preventDefault(),e.setAttribute("src",n.href)});
+//# sourceMappingURL=index.20697a47.js.map
